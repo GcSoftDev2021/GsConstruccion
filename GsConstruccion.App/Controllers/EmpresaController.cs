5d08e933c04e518aa3d3caa6b56e02b90d06a2f6
oscar.castillo <oscar.castillo@LPTOCASTILLO>
2022-01-04 15:07:18 -0500

    Adicionales 1.1

---
+++ GsConstruccion.App/Controllers/EmpresaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GsConstruccion.App.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        // GET: Empresa
+        public ActionResult Empresa()
+        {
+            return View();
+        }
+
+        public ActionResult Crear_Empresa()
+        {
+            return View();
+        }
+
+        public ActionResult Lista_Empresa()
+        {
+            return View();
+        }
+
+        public ActionResult Editar_Empresa()
+        {
+            return View();
+        }
+
+        public ActionResult Detalle_Empresa()
+        {
+            return View();
+        }
+    }
+}
---
+++ GsConstruccion.Data/DataEntities/DataLogin.cs
@@ -0,0 +1,36 @@
+﻿using GsConstruccion.Data.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GsConstruccion.Data.DataEntities
+{
+    public class DataLogin
+    {
+        readonly GsConstruccionEntities _conection = new GsConstruccionEntities();
+        //private readonly DataRol dataRol = new DataRol();
+        public string IniciarSesion(string Usuario, string Password)
+        {
+            string resultado = String.Empty;
+            try
+            {
+                var varUsuario = new SqlParameter("@Usuario", SqlDbType.VarChar) { Value = Usuario };
+                var varPassword = new SqlParameter("@Password", SqlDbType.VarChar) { Value = Password };
+                var varResultado = new SqlParameter("@Resultado", SqlDbType.VarChar) { Direction = ParameterDirection.Output, Size = 255 };
+
+                _conection.Database.ExecuteSqlCommand("SP_IniciarSesion @Usuario, @Password, @Resultado OUTPUT", varUsuario, varPassword, varResultado);
+
+                resultado = Convert.ToString(varResultado.Value);
+            }
+            catch (Exception ex)
+            {
+                resultado = "Error__" + ex.Message;
+            }
+            return resultado;
+        }
+    }
+}
